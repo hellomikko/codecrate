b4a9cbbc57f0bb414964835d12a042f13cb79349
HelloMikko <hellomikko@outlook.com>
2024-10-10 04:11:34 -0400

    Same ol error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -41,8 +41,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        iconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" {...props} />,
-        iconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" {...props} />,
+        IconLeft: (props) => <ChevronLeft className="h-4 w-4" />,
+        IconRight: (props) => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
